784eada566521ec41590b6a77858846dbd9cf044
Emirhan Daglioglu <emirhandaglioglu96@gmail.com>
2021-05-06 01:33:34 +0300

    Added and changed features
    Added  BusinessLayer
    Changed structere of DataLayer

---
+++ ShopApp.BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,26 @@
+﻿using ShopApp.BusinessLayer.Abstract;
+using ShopApp.DataLayer.Abstract;
+using ShopApp.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp.BusinessLayer.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        ICategory _category;
+
+        public CategoryManager(ICategory category)
+        {
+            _category = category;
+        }
+
+        public List<Category> GetList()
+        {
+            throw new NotImplementedException();
+        }  
+    }
+}
